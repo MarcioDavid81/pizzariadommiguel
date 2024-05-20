5c42091270baa8371de28e3489461db2de04bf69
marciodavid <marciodavid81@gmail.com>
2024-05-19 22:40:15 -0300

    feat: add header

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [HeaderComponent, NgOptimizedImage, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
