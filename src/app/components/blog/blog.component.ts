d1b2cf330e54bf45bb5b55b64192349898dde2a7
marciodavid <marciodavid81@gmail.com>
2024-05-26 01:14:06 -0300

    feat: add responsive navbar

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { FooterComponent } from "../footer/footer.component";
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
     selector: 'app-blog',
     standalone: true,
     templateUrl: './blog.component.html',
     styleUrl: './blog.component.css',
-    imports: [HeaderComponent, FooterComponent]
+    imports: [HeaderComponent, FooterComponent, NavbarComponent]
 })
 export class BlogComponent {
 
